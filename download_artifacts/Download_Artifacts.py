e9d3b75414d9778e5a528738962a08a3653fdb94
Huang Cunxin <huangcunxin@outlook.com>
2023-06-06 09:55:21 +0800

    Correct the downloading script and update the truedata

--- download_artifacts/Download_Artifacts.py
+++ download_artifacts/Download_Artifacts.py
@@ -1,32 +1,37 @@
-# This script has a bug. Don't use!
-
 import os
+import io
+import zipfile
 import requests
 from github import Github
 
-# Set your GitHub token and repository details
-GITHUB_TOKEN = "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"
-REPO_OWNER = "OptHuang"
-REPO_NAME = "Option-pricing-regime-jump_matlab"
-WORKFLOW_RUN_ID = 5148212386  # Replace with the desired workflow run ID
+def download_artifacts(token, repo_owner, repo_name, workflow_run_id):
+    # Initialize GitHub API client
+    gh = Github(token)
+    repo = gh.get_repo(f"{repo_owner}/{repo_name}")
 
-# Initialize GitHub API client
-gh = Github(GITHUB_TOKEN)
-repo = gh.get_repo(f"{REPO_OWNER}/{REPO_NAME}")
+    # Get the specified workflow run artifacts
+    workflow_run = repo.get_workflow_run(workflow_run_id)
+    artifacts = workflow_run.get_artifacts()
 
-# Get the specified workflow run artifacts
-workflow_run = repo.get_workflow_run(WORKFLOW_RUN_ID)
-artifacts = workflow_run.get_artifacts()
+    # Get the current file path and set it to the Data_downloaded folder
+    file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Data_downloaded")
+    os.makedirs(file_path, exist_ok=True)
 
-# Get the current file path
-file_path = os.path.dirname(os.path.abspath(__file__))
+    # Download and extract all artifacts
+    for artifact in artifacts:
+        response = requests.get(artifact.archive_download_url, headers={"Authorization": f"token {token}"})
+        
+        if response.status_code == 200:
+            with zipfile.ZipFile(io.BytesIO(response.content)) as zf:
+                zf.extractall(file_path)
+                print(f"Downloaded and extracted {artifact.name}")
+        else:
+            print(f"Error downloading {artifact.name}: {response.status_code}")
 
-# Download all artifacts
-for artifact in artifacts:
-    response = requests.get(artifact.archive_download_url, headers={"Authorization": f"token {GITHUB_TOKEN}"})
-    
-    # Save the .mat file as binary
-    with open(os.path.join(file_path, artifact.name), "wb") as f:
-        f.write(response.content)
+if __name__ == "__main__":
+    GITHUB_TOKEN = "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"
+    REPO_OWNER = "OptHuang"
+    REPO_NAME = "option-pricing-regime-jump_matlab"
+    WORKFLOW_RUN_ID = 5148212386
 
-    print(f"Downloaded {artifact.name}")
+    download_artifacts(GITHUB_TOKEN, REPO_OWNER, REPO_NAME, WORKFLOW_RUN_ID)
